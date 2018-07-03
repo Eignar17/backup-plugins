1f871a09d54e12d53a9f93ae632458e70900bc02
Eignar17 <Eignar17@gmail.com>
2018-07-03 14:17:10 -0500

    Update expand.c

--- simple-animations-compiz-0.8/expand.c
+++ simple-animations-compiz-0.8/expand.c
@@ -11,7 +11,7 @@
  * E-mail    : davidr@novell.com>
  *
  * Particle system added by : (C) 2006 Dennis Kasprzyk
- * E-mail                   : onestone@beryl-project.org
+ * E-mail                   : onestone@compiz.org
  *
  * Beam-Up added by : Florencio Guimaraes
  * E-mail           : florencio@nexcorp.com.br
@@ -110,4 +110,3 @@ fxExpandInit (CompWindow * w)
 
     return (*ad->animBaseFunc->defaultAnimInit) (w);
 }
-
