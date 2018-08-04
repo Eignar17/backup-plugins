8acd0d580328d72fb3b0ac116829f6bdfe1c21ec
Eignar17 <Eignar17@gmail.com>
2018-08-04 17:03:34 -0500

    Update drunken.cpp

--- drunken-compiz-0.8/drunken.cpp
+++ drunken-compiz-0.8/drunken.cpp
@@ -1,7 +1,5 @@
 #include "drunken.h"
 
-COMPIZ_PLUGIN_20090315 (drunken, DrunkenPluginVTable)
-
 bool
 DrunkenWindow::shouldAnimate ()
 {
@@ -20,8 +18,9 @@ DrunkenWindow::shouldAnimate ()
     return true;
 }
 
-void
-DrunkenScreen::preparePaint (int ms)
+static void
+DrunkenPreparePaintScreen (CompScreen *s,
+			    int        ms)
 {
     foreach (CompWindow *w, screen->windows ())
     {
@@ -34,23 +33,36 @@ DrunkenScreen::preparePaint (int ms)
     }
 }
 
-bool
-DrunkenScreen::glPaintOutput (const GLScreenPaintAttrib &attrib,
-			      const GLMatrix	       &transform,
-			      const CompRegion	       &region,
+static Bool
+DrunkenPaintOutput (CompScreen              *s,
+		    	      const ScreenPaintAttrib *sa,
+			      const CompTransform     *origTransform,
+			      Region	               region,
 			      CompOutput	       *output,
 			      unsigned int	       mask)
 {
+    Bool status;
+    CompTransform *mTransform;
+	
+    DRUNK_SCREEN (s);
+{
     mask |= PAINT_SCREEN_WITH_TRANSFORMED_WINDOWS_MASK;
-    
-    return gScreen->glPaintOutput (attrib, transform, region, output, mask);
 }
 
-bool
-DrunkenWindow::glPaint (const GLWindowPaintAttrib &attrib,
-			const GLMatrix		  &transform,
-			const CompRegion	  &region,
-			unsigned int		  mask)
+    UNWRAP (sos, s, paintOutput);
+    status = (*s->paintOutput) (s, sa, mTransform, region, output, mask);
+    WRAP (sos, s, paintOutput, DrunkenPaintOutput);
+     free (mTransform);
+
+     return status;
+}
+
+static Bool
+stereo3dPaintWindow (CompWindow              *w,
+		     const WindowPaintAttrib *attrib,	     
+		     const CompTransform     *transform,
+		     Region                  region,
+		     unsigned int            mask)
 {
     DRUNK_SCREEN (screen);
   
@@ -66,7 +78,7 @@ DrunkenWindow::glPaint (const GLWindowPaintAttrib &attrib,
     
     mask |= PAINT_WINDOW_TRANSFORMED_MASK;
     
-    status = gWindow->glPaint (wAttrib, wTransform1, region, mask);
+   status = gWindow->glPaint (wAttrib, wTransform1, region, mask);
     
     wTransform2.translate (diff, 0.0f, 0.0f);
     
@@ -75,10 +87,11 @@ DrunkenWindow::glPaint (const GLWindowPaintAttrib &attrib,
     return status;
 }
 
-void
-DrunkenScreen::donePaint ()
+static void
+DrunkenDonePaintScreen (CompScreen *s)
 {
-    cScreen->damageScreen ();
+    DRUNK_SCREEN (s);
+        damageScreen (s);
 }
 
 void
@@ -104,37 +117,122 @@ DrunkenScreen::toggle ()
     return true;
 }
 
-DrunkenScreen::DrunkenScreen (CompScreen *screen) :
+static Bool
+DrunkenInitScreen (CompPlugin *p,
+		    CompScreen *s)
+{
     PluginClassHandler <DrunkenScreen, CompScreen> (screen),
     cScreen (CompositeScreen::get (screen)),
     gScreen (GLScreen::get (screen)),
     mEnabled (false)
 {
-    CompositeScreenInterface::setHandler (cScreen, false);
-    GLScreenInterface::setHandler (gScreen, false);
-    
+   
     optionSetInitiateKeyInitiate (boost::bind (&DrunkenScreen::toggle, this));
 }
 
-DrunkenWindow::DrunkenWindow (CompWindow *window) :
-    PluginClassHandler <DrunkenWindow, CompWindow> (window),
-    cWindow (CompositeWindow::get (window)),
-    gWindow (GLWindow::get (window)),
+static void
+DrunkenInitWindow (CompWindow *window)
+{
+    DRUNK_WINDOW(window);
+
+    sow->window = window;
     mDrunkFactor (0)
 {
     bool enabled = DrunkenScreen::get (screen)->mEnabled;
   
-    CompositeWindowInterface::setHandler (cWindow, enabled);
-    GLWindowInterface::setHandler (gWindow, enabled);
 }
 
-bool
-DrunkenPluginVTable::init ()
+static Bool
+DrunkenInitDisplay (CompPlugin  *p,
+		     CompDisplay *d)
 {
-    if (!CompPlugin::checkPluginABI ("core", CORE_ABIVERSION) ||
-	!CompPlugin::checkPluginABI ("composite", COMPIZ_COMPOSITE_ABI) ||
-	!CompPlugin::checkPluginABI ("opengl", COMPIZ_OPENGL_ABI))
-	return false;
-    
-    return true;
+    int index;
+    DrunkenDisplay *sod;
+
+    if (!checkPluginABI ("core", CORE_ABIVERSION))
+        return FALSE;
+
+    sod = (DrunkenDisplay*) malloc (sizeof (DrunkenDisplay));
+    if (!sod)
+        return FALSE;
+
+    sod->screenPrivateIndex = allocateScreenPrivateIndex (d);
+    if (sod->screenPrivateIndex < 0)
+    {
+	free (sod);
+	return FALSE;
+    }
+
+    d->base.privates[displayPrivateIndex].ptr = sod;
+
+    return TRUE;
 }
+
+static void
+DrunkenFiniDisplay (CompPlugin  *p,
+		     CompDisplay *d)
+{
+    DRUNK_DISPLAY (d);
+
+    freeScreenPrivateIndex (d, sod->screenPrivateIndex);
+    free (sod);
+}
+
+static Bool
+DrunkenInit (CompPlugin *p)
+{
+    displayPrivateIndex = allocateDisplayPrivateIndex ();
+
+    if (displayPrivateIndex < 0)
+	return FALSE;
+
+    return TRUE;
+}
+
+static CompBool
+DrunkenInitObject (CompPlugin *p,
+		    CompObject *o)
+{
+    static InitPluginObjectProc dispTab[] = {
+	(InitPluginObjectProc) 0,
+	(InitPluginObjectProc) DrunkenInitDisplay,
+	(InitPluginObjectProc) DrunkenInitScreen
+    };
+
+    RETURN_DISPATCH (o, dispTab, ARRAY_SIZE (dispTab), TRUE, (p, o));
+}
+
+static void
+DrunkenFiniObject (CompPlugin *p,
+		    CompObject *o)
+{
+    static FiniPluginObjectProc dispTab[] = {
+	(FiniPluginObjectProc) 0,
+	(FiniPluginObjectProc) DrunkenFiniDisplay,
+	(FiniPluginObjectProc) DrunkenFiniScreen
+    };
+
+    DISPATCH (o, dispTab, ARRAY_SIZE (dispTab), (p, o));
+}
+
+static void
+DrunkenFini (CompPlugin *p)
+{
+    freeDisplayPrivateIndex (displayPrivateIndex);
+}
+	
+CompPluginVTable DrunkenVTable = {
+{
+    "Drunken",
+    0,
+    DrunkenInit,
+    DrunkenFini,
+    DrunkenInitObject,
+    DrunkenFiniObject,
+    0,
+    0,
+};
+ CompPluginVTable *
+getCompPluginInfo (void)
+{
+    return &stereo3dVTable;
