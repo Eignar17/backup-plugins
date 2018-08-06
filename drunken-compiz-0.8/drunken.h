ac1fce481bd6ba147f1dc325728c772271dd89b1
Eignar17 <Eignar17@gmail.com>
2018-08-06 15:49:12 -0500

    Update drunken.h

--- drunken-compiz-0.8/drunken.h
+++ drunken-compiz-0.8/drunken.h
@@ -27,8 +27,6 @@ typedef struct _DrunkenScreen
 
 };
 
-#define GET_DRUNK_SCREEN(s, ds)                         \
-    ((DrunkenScreen *) (s)->base.privates[(ds)->screenPrivateIndex].ptr)
 
 typedef struct _DrunkenWindow DrunkenWindow;
 	  
@@ -44,8 +42,21 @@ typedef struct _DrunkenWindow DrunkenWindow;
 	  float	drunkenGetFactor;
 };
 
+#define GET_STEREO3D_DISPLAY(d)                            \
+    ((DrunkeDisplay *) (d)->base.privates[displayPrivateIndex].ptr)
+
+
+#define GET_DRUNK_SCREEN(s, ds)                         \
+    ((DrunkenScreen *) (s)->base.privates[(ds)->screenPrivateIndex].ptr)
+
 #define GET_DRUNK_WINDOW(w, dw)                         \
     ((DrunkenWindow *) (w)->base.privates[(dw)->windowPrivateIndex].ptr)
+
+#define DRUNK_DISPLAY(d)						       \
+    Stereo3DDisplay *sod = GET_DRUNK_DISPLAY (d)
+
+#define DRUNK_SCREEN(s)						       \
+    Stereo3DScreen *ds = GET_DRUNK_SCREEN (s, GET_DRUNK_DISPLAY (s->display))
 
 #define DRUNK_WINDOW(w)							\
     DrunkenWindow *dw = GET_DRUNK_WINDOW (w, GET_DRUNK_SCREEN (w->screen, GET_SDRUNK_DISPLAY (w->screen->display)))
