6586b3ae9ec9dd21b33792333a0f55c45165504c
eignar samaniego <Eignar17@gmail.com>
2023-01-16 03:54:55 -0500

    Update drunken.h

--- drunken-compiz-0.8/drunken.h
+++ drunken-compiz-0.8/drunken.h
@@ -44,19 +44,19 @@ typedef struct _DrunkenWindow DrunkenWindow;
 #define GET_STEREO3D_DISPLAY(d)                            \
     ((DrunkeDisplay *) (d)->base.privates[displayPrivateIndex].ptr)
 
-#define GET_DRUNK_SCREEN(s, ds)                         \
+#define GET_DRUNK_SCREEN(s, drs)                         \
     ((DrunkenScreen *) (s)->base.privates[(ds)->screenPrivateIndex].ptr)
 
-#define GET_DRUNK_WINDOW(w, dw)                         \
+#define GET_DRUNK_WINDOW(w, drw)                         \
     ((DrunkenWindow *) (w)->base.privates[(dw)->windowPrivateIndex].ptr)
 
 #define DRUNK_DISPLAY(d)						       \
-    DrunkenDisplay *ds = GET_DRUNK_DISPLAY (d)
+    DrunkenDisplay *drs = GET_DRUNK_DISPLAY (d)
 
 #define DRUNK_SCREEN(s)						       \
-   DrunkenScreen *ds = GET_DRUNK_SCREEN (s, GET_DRUNK_DISPLAY (s->display))
+   DrunkenScreen *drs = GET_DRUNK_SCREEN (s, GET_DRUNK_DISPLAY (s->display))
 
 #define DRUNK_WINDOW(w)							\
-    DrunkenWindow *dw = GET_DRUNK_WINDOW (w, GET_DRUNK_SCREEN (w->screen, GET_DRUNK_DISPLAY (w->screen->display)))
+    DrunkenWindow *drw = GET_DRUNK_WINDOW (w, GET_DRUNK_SCREEN (w->screen, GET_DRUNK_DISPLAY (w->screen->display)))
 
 #endif
