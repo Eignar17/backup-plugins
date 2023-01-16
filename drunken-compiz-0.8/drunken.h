9e7d61a5da508841d2c6bf2d517627aa851171a7
eignar samaniego <Eignar17@gmail.com>
2023-01-16 09:39:57 -0500

    Update drunken.h

--- drunken-compiz-0.8/drunken.h
+++ drunken-compiz-0.8/drunken.h
@@ -47,16 +47,16 @@ typedef struct _DrunkenWindow DrunkenWindow;
 #define GET_DRUNK_SCREEN(s, drs)                         \
     ((DrunkenScreen *) (s)->base.privates[(ds)->screenPrivateIndex].ptr)
 
-#define GET_DRUNK_WINDOW(w, drw)                         \
+#define GET_DRUNK_WINDOW(w, dw)                           \
     ((DrunkenWindow *) (w)->base.privates[(dw)->windowPrivateIndex].ptr)
 
 #define DRUNK_DISPLAY(d)						       \
-    DrunkenDisplay *drd = GET_DRUNK_DISPLAY (d)
+    DrunkenDisplay *dd = GET_DRUNK_DISPLAY (d)
 
 #define DRUNK_SCREEN(s)						       \
-   DrunkenScreen *drs = GET_DRUNK_SCREEN (s, GET_DRUNK_DISPLAY (s->display))
+   DrunkenScreen *ds = GET_DRUNK_SCREEN (s, GET_DRUNK_DISPLAY (s->display))
 
 #define DRUNK_WINDOW(w)							\
-    DrunkenWindow *drw = GET_DRUNK_WINDOW (w, GET_DRUNK_SCREEN (w->screen, GET_DRUNK_DISPLAY (w->screen->display)))
+    DrunkenWindow *dw = GET_DRUNK_WINDOW (w, GET_DRUNK_SCREEN (w->screen, GET_DRUNK_DISPLAY (w->screen->display)))
 
 #endif
